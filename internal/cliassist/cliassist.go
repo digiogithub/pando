91a22ed4ac1ee2dfaf78de5cc5f7b544302328d2
Jose Francisco Rives Lirola <jose@digio.es>
2026-03-19 23:07:58 +0100

    feat: cli-assist mode

---
+++ internal/cliassist/cliassist.go
@@ -0,0 +1,13 @@
+package cliassist
+
+import (
+	"context"
+
+	"github.com/digiogithub/pando/internal/config"
+)
+
+// Run is the entrypoint for cli-assist mode. Will be fully implemented in Phase 5.
+func Run(ctx context.Context, cfg *config.Config, args []string) {
+	// TODO: implemented in later phases
+	panic("cliassist.Run not yet implemented")
+}
